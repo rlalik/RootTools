0a00d3970532e40121808d06b9d5c6960e2a9b7f
Rafal Lalik <RafalLalik@ph.tum.de>
2015-09-17 14:46:50 +0200

    tests

---
+++ tests/basic_tests.cpp
@@ -0,0 +1,46 @@
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <RootTools.h>
+#include <TH1.h>
+
+class BasicCase : public CPPUNIT_NS::TestFixture
+{
+	CPPUNIT_TEST_SUITE( BasicCase );
+	CPPUNIT_TEST( MyTest );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+
+protected:
+	void MyTest();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION( BasicCase );
+
+void BasicCase::setUp()
+{
+}
+
+void BasicCase::MyTest()
+{
+// 	float fnum = 2.00001f;
+// // 	CPPUNIT_FAIL("zxczc");
+// 	CPPUNIT_ASSERT_DOUBLES_EQUAL( fnum, 2.0f, 0.0005 );
+// 
+// 	std::string pattern_string("%%d pattern");
+// 	std::string test_string("test pattern");
+// 	std::string replace_string("test");
+// 
+// 	std::string output_string = SmartFactory::placeholder(pattern_string, "%%d", replace_string);
+// 	CPPUNIT_ASSERT_EQUAL(output_string, test_string);
+
+	TH1D * h = new TH1D("h_errors_test", "h", 100, -5, 5);
+	h->FillRandom("gaus", 100000);
+
+	int bins = h->GetXaxis()->GetNbins();
+	double err1 = RootTools::calcTotalError(h, 0, bins);
+	double err2 = RootTools::calcTotalError2(h, 0, bins);
+
+	printf(" err1 = %g\t err2 = %g\n", err1, err2);
+}
